2a4fbd62642987a96b613006836a3287f3bd2bbd
Diana <dele18607@uvg.edu.gt>
2021-02-03 12:02:49 -0600

    empezando

---
+++ tile.py
@@ -0,0 +1,70 @@
+# index 0 is tile type
+#   0 = blank normal tile
+#   1 = blank green goal tile
+#   2 = blank red goal tile
+#
+# index 1 is piece type
+#   0 = no piece
+#   1 = green piece
+#   2 = red piece
+#
+# index 2 is the outline type
+#   0 = no outline
+#   1 = selected outline
+#   2 = just moved
+
+
+class Tile():
+
+    # Goal constants
+    T_NONE = 0
+    T_GREEN = 1
+    T_RED = 2
+
+    # Piece constants
+    P_NONE = 0
+    P_GREEN = 1
+    P_RED = 2
+
+    # Outline constants
+    O_NONE = 0
+    O_SELECT = 1
+    O_MOVED = 2
+
+    def __init__(self, tile=0, piece=0, outline=0, row=0, col=0):
+        self.tile = tile
+        """print("--------------------------")
+        print(self.tile)"""
+        self.piece = piece
+        #print(self.piece)
+        self.outline = outline
+
+        self.row = row
+        self.col = col
+        self.loc = (row, col)
+
+    def get_tile_colors(self):
+
+        # Find appropriate tile color
+        tile_colors = [
+            ("#8C6C50", "#DBBFA0"),  # Normal tiles
+            ("#71b651", "#a6ce9d"),  # Red goal tiles
+            ("#ba6262", "#ce9d9d")   # Green goal tiles
+        ]
+        tile_color = tile_colors[self.tile][(self.loc[0] + self.loc[1]) % 2]
+
+        # Find appropriate outline color
+        outline_colors = [
+            tile_color,
+            "yellow",  # TODO: Change
+            "#1100BB"
+        ]
+        outline_color = outline_colors[self.outline]
+
+        return tile_color, outline_color
+
+    def __str__(self):
+        return chr(self.loc[1] + 97) + str(self.loc[0] + 1)
+
+    def __repr__(self):
+        return chr(self.loc[1] + 97) + str(self.loc[0] + 1)
